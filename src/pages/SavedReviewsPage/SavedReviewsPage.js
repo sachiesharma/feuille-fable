b0ac00a4e0dcd643615f509cc310faef3f101492
Sachie Sharma <sachie@gmail.com>
2024-02-22 20:34:30 -0500

    added the saved-review-page

---
+++ src/pages/SavedReviewsPage/SavedReviewsPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../SavedReviewsPage/SavedReviewsPage.scss";
+
+function SavedReviewsPage() {
+  return <div>SavedReviewsPage</div>;
+}
+
+export default SavedReviewsPage;
