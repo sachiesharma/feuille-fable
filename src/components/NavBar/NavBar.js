3049f1439db59434d9a804a1e235530451f536b4
Sachie Sharma <sachie@gmail.com>
2024-02-26 01:02:26 -0500

    changed logo to png

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "../NavBar/NavBar.scss";
 import { Link } from "react-router-dom";
-// import ffLogo from "../../assets/images/feuillefablelogo.svg";
+import ffLogo from "../../assets/images/feuille-fable-logo.png";
 
 function NavBar() {
   return (
-    <ul className="navbar">
-      {/* <li>
-        <img src={ffLogo} alt="Feuille-Fable Logo" />
-      </li> */}
-      <li className="navbar__item">
-        <Link to="/">Home</Link>
-      </li>
-      <li className="navbar__item">
-        <Link to="/saved-reviews">Entries</Link>
-      </li>
-    </ul>
+    <div>
+      <img src={ffLogo} alt="Feuille-Fable Logo" />
+      <ul className="navbar">
+        <li className="navbar__item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navbar__item">
+          <Link to="/saved-reviews">Entries</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 
