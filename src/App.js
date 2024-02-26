9c35a9ac309c7a7a9f45ab93ca01f4723b5db60a
Sachie Sharma <sachie@gmail.com>
2024-02-25 21:39:11 -0500

    added styling and list

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.js";
 import BookReviewPage from "./pages/BookReviewPage/BookReviewPage.js";
 import SavedReviewsPage from "./pages/SavedReviewsPage/SavedReviewsPage.js";
+import NavBar from "./components/NavBar/NavBar.js";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/book-review/works/:bookId" element={<BookReviewPage />} />
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../NavBar/NavBar.scss";
+
+function NavBar() {
+  return (
+    // <p>FeuilleFable</p>
+    <ul className="navbar">
+      <li className="navbar__item">Home</li>
+      <li className="navbar__item">Entries</li>
+    </ul>
+  );
+}
+
+export default NavBar;
