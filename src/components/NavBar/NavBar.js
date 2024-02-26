c5d10d58cd9cd1b3c2753aad62c47c003807f1fe
Sachie Sharma <sachie@gmail.com>
2024-02-26 01:23:49 -0500

    nav bar styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "../NavBar/NavBar.scss";
 import { Link } from "react-router-dom";
-import ffLogo from "../../assets/images/feuille-fable-logo.png";
+import ffLogo from "../../assets/images/feuille-fable-logo-good.png";
 
 function NavBar() {
   return (
     <div>
-      <img src={ffLogo} alt="Feuille-Fable Logo" />
       <ul className="navbar">
-        <li className="navbar__item">
-          <Link to="/">Home</Link>
+        <li>
+          <img className="navbar__logo" src={ffLogo} alt="Feuille-Fable Logo" />
         </li>
-        <li className="navbar__item">
-          <Link to="/saved-reviews">Entries</Link>
-        </li>
+        <div className="navbar__menu">
+          <li className="navbar__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navbar__item">
+            <Link to="/saved-reviews">Entries</Link>
+          </li>
+        </div>
       </ul>
     </div>
   );
