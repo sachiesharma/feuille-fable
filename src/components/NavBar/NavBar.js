6e14b10afb3ad2aae0ea8b7cba74f977e01f9f91
Sachie Sharma <sachie@gmail.com>
2024-02-25 23:13:49 -0500

    created and edited nav-bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "../NavBar/NavBar.scss";
+import { Link } from "react-router-dom";
+// import ffLogo from "../../assets/images/feuillefablelogo.svg";
 
 function NavBar() {
   return (
-    // <p>FeuilleFable</p>
     <ul className="navbar">
-      <li className="navbar__item">Home</li>
-      <li className="navbar__item">Entries</li>
+      {/* <li>
+        <img src={ffLogo} alt="Feuille-Fable Logo" />
+      </li> */}
+      <li className="navbar__item">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="navbar__item">
+        <Link to="/saved-reviews">Entries</Link>
+      </li>
     </ul>
   );
 }
